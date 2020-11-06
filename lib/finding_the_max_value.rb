35ced4251fb8caeb3c0b2afce12b7c29876dab07
Noura Loudani <contact.nouraloudani@gmail.com>
2020-11-06 07:05:19 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,7 +1,7 @@
 def find_max_value(array)
   count = 0
   max_value = -1
-  while i < array length do 
+  while count < array length do 
     if max_value < count[i]
       max_value =count[i]
     end
